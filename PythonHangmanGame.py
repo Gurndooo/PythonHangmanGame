e9a91bc44dfd9afb2c4b9df251847db7b75b7b31
Gurndooo <gurndoo@gmail.com>
2024-10-10 15:25:56 -0400

    Test run

---
+++ PythonHangmanGame.py
@@ -0,0 +1,20 @@
+import pprint
+
+sentence = "This is a common interview question"
+
+char_frequency = {}
+
+for char in sentence:
+    if char in char_frequency:
+        char_frequency[char] += 1
+    else:
+        char_frequency[char] = 1
+
+pprint.pprint(char_frequency, width=1)
+
+pprint.pprint(sorted(
+    char_frequency.items(),
+    key = (lambda kv: kv[1]),
+    reverse = True), 
+    width=10
+)
