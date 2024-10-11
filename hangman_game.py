a34c0f4032cba9d75bcd70e9af96fe35ecac58e5
Gurndooo <gurndoo@gmail.com>
2024-10-11 15:14:56 -0400

    Function named

--- hangman_game.py
+++ hangman_game.py
@@ -1,6 +1,15 @@
-import tkinter
+import random
 
+#list of potential secret words
+word_list = ["python", "hangman", "programming", "challenge"]
+#Randomly select a secret word from the list
+secret_word = random.choice(list)
+#Initialize variables to keep track of guesses and attempts
+correct_guesses = set()
+incorrect_guesses = set()
+attempts_left = 6
 
-#1st commit
-
+#Function to display the current game state
+def display_game_state():
+    #Display the secret word with guessed letters revealed
 
